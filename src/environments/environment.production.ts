84bc7616f0f62a18741f8fe6c8a6ba58f6823185
moses12345 <shaikmoses1997@gmail.com>
2023-08-18 12:18:13 +0530

    few changes

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyBvQH9MG1ZbCirAYTm2l5sAd8If2nfo3nE',
     authDomain: 'raksa-1e906.firebaseapp.com',
